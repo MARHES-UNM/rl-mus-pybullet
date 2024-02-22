e08b34ce6d7ba77f6cd94bd8e40a3abaf3f51052
Jean-Elie Pierre <gno320@gmail.com>
2024-02-21 23:50:33 -0500

    removed items

--- scripts/train_uavs.sh
+++ scripts/train_uavs.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+#TODO: need to update
 
 python multi_agent_shared_parameter.py --name low_stp --stop-timesteps 20000000
 python train_agent.py --name low_stp --stop-timesteps 20000000

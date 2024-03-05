0abcb5079adad4f215d8813f660c8b73acb0f82c
Jean-Elie Pierre <gno320@gmail.com>
2024-03-05 00:07:19 -0500

    minus atp reward

--- run_experiment.py
+++ run_experiment.py
@@ -112,8 +112,8 @@ def train(args):
     # args.config["env_config"]["env_max_h"] = 4
     # args.config["env_config"]["env_max_l"] = 4
     # args.config["env_config"]["env_max_w"] = 4
-    args.config["env_config"]["tgt_reward"] = tune.grid_search([10, 1, 0, 0.1, 50])
-    args.config["env_config"]["stp_penalty"] = tune.grid_search([5, 0, .1])
+    args.config["env_config"]["tgt_reward"] = tune.grid_search([10])
+    args.config["env_config"]["stp_penalty"] = tune.grid_search([5, .1])
     args.config["env_config"]["d_thresh"] = tune.grid_search([0.15])
     args.config["env_config"]["time_final"] = tune.grid_search([8])
     args.config["env_config"]["t_go_max"] = tune.grid_search([2.0])
--- train_uavs.sh
+++ train_uavs.sh
@@ -2,5 +2,5 @@
 
 
 # python multi_agent_shared_parameter.py --name low_stp --stop-timesteps 20000000
-python run_experiment.py --env_name "rl-mus-ttc-v0" --name only_tune_stp_pen train --stop_timesteps 20000000
+python run_experiment.py --env_name "rl-mus-ttc-v0" --name tgt_rew_param_study train --stop_timesteps 20000000
 # python run_experiment.py --env_name "rl-mus-v0" --name rlmus_baseline train --stop_timesteps 20000000
